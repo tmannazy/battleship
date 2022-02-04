c0fdf690719f4e4ba3bda67203b9ed5381acb6e9
tmannazy <e.anene@hotmail.com>
2022-02-04 16:20:39 +0100

    Refactor codebase to test code

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,18 +1,25 @@
 import Ship from "../codes/ship";
 
-test("player's ship was hit correctly", () => {
-  const shipUnderTest = Ship();
-  expect(shipUnderTest.hit(2)).toBe("hit");
-});
+describe("test the ship factory behaviour", () => {
+  const shipUnderTest = Ship(5);
 
-test("takes a number and mark the position hit", () => {
-  const shipUnderTest = Ship();
-  expect(shipUnderTest.length(4)).toEqual(4);
-});
+  test("the correct length of a ship", () => {
+    expect(shipUnderTest.length).toEqual(5);
+  });
 
-test("opponent ship totally destroyed", () => {
-  const shipUnderTest = Ship();
-  expect(shipUnderTest.isSunk(4)).toEqual(
-    expect(shipUnderTest.length(4)).toBe(true)
-  );
+  test("player's ship was hit correctly", () => {
+    expect(shipUnderTest.hit(2)).toBe("hit");
+  });
+
+  test("takes a number and mark the position hit", () => {
+    const shipUnderTest = Ship();
+    expect(shipUnderTest.length(4)).toEqual(4);
+  });
+
+  test("opponent ship totally destroyed", () => {
+    const shipUnderTest = Ship();
+    expect(shipUnderTest.isSunk(4)).toEqual(
+      expect(shipUnderTest.length(4)).toBe(true)
+    );
+  });
 });
