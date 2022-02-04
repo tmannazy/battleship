0b55bb02ad6a2c053b86e19b8b90625300644f96
tmannazy <e.anene@hotmail.com>
2022-02-04 23:12:46 +0100

    Test for ship isSunk() position completely hit.

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,25 +1,30 @@
 import Ship from "../codes/ship";
 
-describe("test the ship factory behaviour", () => {
+describe("test the ship factory behavior", () => {
   const shipUnderTest = Ship(5);
 
   test("the correct length of a ship", () => {
     expect(shipUnderTest.length).toEqual(5);
   });
 
-  test("player's ship was hit correctly", () => {
-    expect(shipUnderTest.hit(2)).toBe("hit");
+  test("player's ship hit targeted position correctly", () => {
+    shipUnderTest.hit(2);
+    expect(shipUnderTest.getShipArray().at(2)).toBe("hit");
   });
 
-  test("takes a number and mark the position hit", () => {
-    const shipUnderTest = Ship();
-    expect(shipUnderTest.length(4)).toEqual(4);
-  });
-
-  test("opponent ship totally destroyed", () => {
-    const shipUnderTest = Ship();
-    expect(shipUnderTest.isSunk(4)).toEqual(
-      expect(shipUnderTest.length(4)).toBe(true)
-    );
+  test("when ship sinks, ship array must be correctly marked and isSunk should be true", () => {
+    shipUnderTest.hit(0);
+    shipUnderTest.hit(1);
+    shipUnderTest.hit(2);
+    shipUnderTest.hit(3);
+    shipUnderTest.hit(4);
+    expect(shipUnderTest.getShipArray()).toEqual([
+      "hit",
+      "hit",
+      "hit",
+      "hit",
+      "hit",
+    ]);
+    expect(shipUnderTest.isSunk()).toBe(true);
   });
 });
