a2e3e2604833d30699e26afe4fb32f787379d003
tmannazy <e.anene@hotmail.com>
2022-02-08 23:06:06 +0100

    Create a test suite for gameboard factory.

---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,6 @@
+import Ship from "../codes/ship";
+import Gameboard from "../codes/gameboard";
+
+describe("test the gameboard factory behavior for", () => {
+  const board = Gameboard(cordX, cordY);
+});
